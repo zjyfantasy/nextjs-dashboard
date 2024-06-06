0ad6dd70cbd81a84e873e08628119fe33f21adba
zhongjinyan <zhongjinyan@biad.com.cn>
2024-06-06 16:35:52 +0800

    添加pdf预览

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import jsPreviewPdf from "@js-preview/pdf";
 import styles from "./page.module.css";
 
 const page = () => {
-  return <div className={styles.container}>dashboard</div>;
+  useEffect(() => {
+    const myPdfPreviewer = jsPreviewPdf.init(document.getElementById("pdf"), {
+      onError: (e) => {
+        console.log("发生错误", e);
+      },
+      onRendered: () => {
+        console.log("渲染完成");
+      },
+    });
+    myPdfPreviewer.preview("/math.pdf");
+  }, []);
+  return (
+    <div className={styles.container}>
+      <div id="pdf"></div>
+    </div>
+  );
 };
 
 export default page;
